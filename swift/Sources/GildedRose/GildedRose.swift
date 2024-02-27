c5a1e8195c7f6a800be2ea779b9d5e505e87e261
Nicolá Domingues <nico.domingues.dev@gmail.com>
2024-02-27 20:50:31 +0000

    updateQuality does not change Sulfuras Items

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -7,11 +7,11 @@ public class GildedRose {
 
     public func updateQuality() {
         for i in 0 ..< items.count {
+          guard items[i].name != "Sulfuras, Hand of Ragnaros" else { continue }
+          
             if items[i].name != "Aged Brie", items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
                 if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
-                    }
+                  items[i].quality = items[i].quality - 1
                 }
             } else {
                 if items[i].quality < 50 {
@@ -33,17 +33,13 @@ public class GildedRose {
                 }
             }
 
-            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                items[i].sellIn = items[i].sellIn - 1
-            }
+          items[i].sellIn = items[i].sellIn - 1
 
             if items[i].sellIn < 0 {
                 if items[i].name != "Aged Brie" {
                     if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
                         if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
+                          items[i].quality = items[i].quality - 1
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality
--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -2,10 +2,13 @@
 import XCTest
 
 class GildedRoseTests: XCTestCase {
-    func testFoo() throws {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
-    }
+  
+  func test_updateQuality_doesNotChangeSulfurasItems() {
+    let items = [Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80)]
+    let app = GildedRose(items: items)
+    app.updateQuality()
+    XCTAssertEqual(app.items[0].name, items[0].name)
+    XCTAssertEqual(app.items[0].sellIn, items[0].sellIn)
+    XCTAssertEqual(app.items[0].quality, items[0].quality)
+  }
 }
