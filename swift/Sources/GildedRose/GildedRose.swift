61eea6059d65f8c4120d55adb969c19b617f790b
Nicolá Domingues <nico.domingues.dev@gmail.com>
2024-02-27 20:55:50 +0000

    Indent code

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -1,55 +1,55 @@
 public class GildedRose {
-    var items: [Item]
-
-    public init(items: [Item]) {
-        self.items = items
-    }
-
-    public func updateQuality() {
-      for item in items {
-          guard item.name != "Sulfuras, Hand of Ragnaros" else { continue }
-          
-            if item.name != "Aged Brie", item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                if item.quality > 0 {
-                  item.quality = item.quality - 1
-                }
-            } else {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1
-
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.sellIn < 11 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if item.sellIn < 6 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-          item.sellIn = item.sellIn - 1
-
-            if item.sellIn < 0 {
-                if item.name != "Aged Brie" {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.quality > 0 {
-                          item.quality = item.quality - 1
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1
-                    }
-                }
-            }
+  var items: [Item]
+  
+  public init(items: [Item]) {
+    self.items = items
+  }
+  
+  public func updateQuality() {
+    for item in items {
+      guard item.name != "Sulfuras, Hand of Ragnaros" else { continue }
+      
+      if item.name != "Aged Brie", item.name != "Backstage passes to a TAFKAL80ETC concert" {
+        if item.quality > 0 {
+          item.quality = item.quality - 1
         }
+      } else {
+        if item.quality < 50 {
+          item.quality = item.quality + 1
+          
+          if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+            if item.sellIn < 11 {
+              if item.quality < 50 {
+                item.quality = item.quality + 1
+              }
+            }
+            
+            if item.sellIn < 6 {
+              if item.quality < 50 {
+                item.quality = item.quality + 1
+              }
+            }
+          }
+        }
+      }
+      
+      item.sellIn = item.sellIn - 1
+      
+      if item.sellIn < 0 {
+        if item.name != "Aged Brie" {
+          if item.name != "Backstage passes to a TAFKAL80ETC concert" {
+            if item.quality > 0 {
+              item.quality = item.quality - 1
+            }
+          } else {
+            item.quality = item.quality - item.quality
+          }
+        } else {
+          if item.quality < 50 {
+            item.quality = item.quality + 1
+          }
+        }
+      }
     }
+  }
 }
